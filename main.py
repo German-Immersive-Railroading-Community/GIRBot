c31ffb011cb6b4e0ef07a4449775a13a2b04ac6f
Raziel Kretschmer <kretschmer.raziel@gmail.com>
2021-07-09 23:21:38 +0200

    Set up slash command for applications, waiting for PR from database-branch

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ bot_prefix = "ANiceAndLongPrefixSoNoOneUsesIt"
 token = config("token")
 client = cmd.Bot(command_prefix=bot_prefix, intents=dc.Intents.all())
 slash = dcs.SlashCommand(client, sync_commands=True)
+sent_app_channel_id = config("sent_app_channel_id")
 
 guild_id = str(config("guild_id")).split(",")
 guild_id = [int(i) for i in guild_id]
@@ -32,7 +33,6 @@ girc_guild_id = int(guild_id[0])
 
 head_dev_role_id = int(config("head_dev_role_id"))
 dev_role_id = int(config("dev_role_id"))
-everyone_role = config("everyone_role_id")
 
 # Standard shit
 
@@ -85,7 +85,33 @@ async def devset(ctx, person, language):
     await person.add_roles(ctx.guild.get_role(role_id=dev_role_id))
     await person.add_roles(dc.utils.get(ctx.guild.roles,
                                         name=language + " Member"))
-    await ctx.send(content=f"Dem Nutzer {person.display_name} wurde die Developer-Rolle gegeben!")
+    await ctx.send(content=f"Dem Nutzer {person.display_name} wurde die Developer-Rolle gegeben!", hidden=True)
 
+
+# Commands with DB use
+
+@slash.slash(
+    name="Apply",
+    description="Apply for a Role on the server.",
+    guild_ids=guild_id,
+    options=[
+        create_option(
+            name="role",
+            description="The role you want to apply for.",
+            option_type=8,
+            required=True
+        ),
+        create_option(
+            name="text",
+            description="A nice little text why you want the role.",
+            option_type=3,
+            required=False
+        )
+    ]
+)
+async def application(ctx, role, text):
+    #TODO Check in Class 'Db_interface' function is_member = True, else cancel with message
+    #TODO Check in Class 'Db_interface' function count_application > 3, cancel with message
+    print("Placeholder")
 
 client.run(token)
