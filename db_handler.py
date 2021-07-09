0755cd72441a9d49a5f1b68463ff4b9b9bcfb2ab
Kontiko <kontiko.fb@gmail.com>
2021-07-09 22:01:10 +0200

    Createt new Branch

--- db_handler.py
+++ db_handler.py
@@ -2,13 +2,21 @@ import mysql.connector as sql
 from decouple import config
 
 # Connecting and setting up SQL
-db = sql.connect(
-    host=str(config("host")),
-    user=str(config("db_user")),
-    password=str(config("db_password")),
-    database=str(config("db_name"))
-    )
+class Db_interface:
+
+    def __init__(self):
+        self.db = sql.connect(
+            host=str(config("host")),
+            user=str(config("db_user")),
+            password=str(config("db_password")),
+            database=str(config("db_name"))
+            )
+        self.cursor = self.db.cursor()
+    def status(self):
+        self.cursor.execute("SHOW TABLES")
+        return self.cursor.fetchall()
 
 if __name__ == "__main__":
-    print("Please run main.py, not this file!")
+    db_int=Db_interface()
+    print(db_int.status())
     exit()
