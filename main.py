6cff1cf65c6a4f49323a0192ccf3996d01e6dc21
FGBxRamel <kretschmer.raziel@gmail.com>
2022-03-27 17:59:13 +0200

    test: if this works I'm gonna cry

--- main.py
+++ main.py
@@ -237,7 +237,7 @@ async def vote(ctx, id, vote):
     scope=girc_guild_id,
 )
 async def test(ctx):
-    print(ctx.data, ctx.type, ctx.data.type)
+    print(int(ctx.user.id))
     await ctx.send(content="Nothing to see here!", ephemeral=True)
 
 async def get_role_from_name(role_name, guild_id = girc_guild_id):
